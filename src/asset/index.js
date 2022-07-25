e795a40cb407a03b1c9a87e55cb36ef1a498cc51
maitreyee nath <maitreyeenath30798@gmail.com>
2022-07-25 16:31:49 +0530

    fix: Fixed css and removed unused images
    Fixed profile page button styling
    Removed unused images

--- src/asset/index.js
+++ src/asset/index.js
@@ -2,19 +2,34 @@ import img01 from "./img01.jpg";
 import img04 from "./img04.jpg";
 import img05 from "./img05.jpg";
 import img06 from "./img06.jpg";
-import img9 from "./img9.jpg";
-import img10 from "./img10.jpg";
 import img11 from "./img11.jpg";
-import img12 from "./img12.jpg"; 
+import img12 from "./img12.jpg";
 import img13 from "./img13.jpg";
 import img14 from "./img14.jpg";
 import img15 from "./img15.jpg";
 import img16 from "./img16.jpg";
 import img17 from "./img17.jpg";
 import img18 from "./img18.jpg";
-import errorimg from "./error-img.svg"
-import errorproductimg from "./error-product-img.svg"
-import emptywishlist from "./emptywishlist.svg"
-import emptycart from "./emptycart.svg"
+import errorimg from "./error-img.svg";
+import errorproductimg from "./error-product-img.svg";
+import emptywishlist from "./emptywishlist.svg";
+import emptycart from "./emptycart.svg";
 
-export { img01,img04,img05,img06,img9,img10,img11,img12,img13,img14,img15,img16,img17,img18,errorimg,errorproductimg,emptywishlist,emptycart }
+export {
+  img01,
+  img04,
+  img05,
+  img06,
+  img11,
+  img12,
+  img13,
+  img14,
+  img15,
+  img16,
+  img17,
+  img18,
+  errorimg,
+  errorproductimg,
+  emptywishlist,
+  emptycart,
+};
